49772a6a66bb46170ecfc1c435b7cce54ae642bd
Ваше Имя <you@example.com>
2025-03-15 04:23:53 +0200

    add service

--- wp-content/themes/ni-legal/services/MsApi.php
+++ wp-content/themes/ni-legal/services/MsApi.php
@@ -10,38 +10,85 @@ class MsApi
 
         return NI_LEGAL_MS_API_HOST;
     }
+
+    protected static function request($path, $data, $accessToken) {
+        $curl = null;
+
+        $host = static::getHost();
+
+        try {
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => $host . $path . http_build_query($data),
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'GET',
+
+                CURLOPT_HTTPHEADER => array(
+                    'Authorization: Bearer ' . $accessToken,
+                    'Content-Type: application/json'
+                ),
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+
+            return json_decode($response);
+        } catch (\Exception $e) {} finally {
+            curl_close($curl);
+
+            return null;
+        }
+    }
+
     public static function getTransactionDetails($email, $accessToken)
     {
-        static::getHost();
-        $curl = curl_init();
+        return static::request(
+            '/api/v1/transaction/details?',
+            ['email' => $email],
+            $accessToken
+        );
+    }
 
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => NI_LEGAL_MS_API_HOST . '/api/v1/transaction/details?' . http_build_query(['email' => $email]),
-//					CURLOPT_URL => 'https://wn.azurewebsites.net/api/Matter/GetClientTransactionDetails',
-            //CURLOPT_URL => 'https://wnpreprod.focisportal.co.uk/API/api/Matter/GetClientTransactionDetails',
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'GET',
-//					 CURLOPT_POSTFIELDS => '"JosephBloggs@TestAccount.co.uk"',
-            // CURLOPT_POSTFIELDS => '"phigginsontestbroker1@wilson-nesbitt.co.uk"',
-            // CURLOPT_POSTFIELDS => '"testing104@wilson-nesbitt.co.uk"',
-//					CURLOPT_POSTFIELDS => '"'.$username.'"',
-            // CURLOPT_POSTFIELDS => '"'.$email_value.'"',
+    public static function getBrokerListing($email, $accessToken)
+    {
+        return static::request(
+            '/api/v1/broker?',
+            ['email' => $email],
+            $accessToken
+        );
+    }
 
-            CURLOPT_HTTPHEADER => array(
-                'Authorization: Bearer ' . $accessToken,
-                'Content-Type: application/json'
-            ),
-        ));
+    public static function getMatterDetails($matterId, $accessToken)
+    {
+        return static::request(
+            '/api/v1/matter/' . $matterId,
+            [],
+            $accessToken
+        );
+    }
 
-        $response = curl_exec($curl);
+    public static function getMatterHistory($matterId, $accessToken)
+    {
+        return static::request(
+            '/api/v1/matter/'.$matterId.'/history/',
+            [],
+            $accessToken
+        );
+    }
 
-        curl_close($curl);
-
-        return json_decode($response);
+    public static function getMilestoneDates($matterId, $accessToken)
+    {
+        return static::request(
+            '/api/v1/matter/'.$matterId.'/milestone-dates',
+            [],
+            $accessToken
+        );
     }
 }
